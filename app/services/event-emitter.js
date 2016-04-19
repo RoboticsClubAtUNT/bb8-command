49c841b06a8bb9ecf1f485d14da4cefbc091754d
Charles Bido <charlesbido@me.com>
2016-04-19 01:31:56 -0500

    wrapped ipcRenderer into ember service

---
+++ app/services/event-emitter.js
@@ -0,0 +1,29 @@
+import Ember from 'ember';
+const ipc = require('electron').ipcRenderer;
+
+export default Ember.Service.extend({
+  /**
+   * This will `emit` a message to the main process.
+   * http://electron.atom.io/docs/v0.37.6/api/ipc-main/
+   * @param  {string} event The channel to be used for communication.
+   * @param  {string} data  JSON data that has been stringified.
+   * @return {[type]}       [description]
+   */
+  emit(event, data) {
+    ipc.send(event, data);
+  },
+
+  /**
+   * This will `listen` for a message from the main process.
+   * http://electron.atom.io/docs/v0.37.6/api/ipc-main/
+   * @param  {string}   event    The channel to be used for communication.
+   * @param  {function} callback A callback function that expects a 'data' as
+   *                             a paramater. (function(res, data){})(res, data);
+   * @return {[type]}            [description]
+   */
+  on(event, callback) {
+    ipc.on(event, function(res, data) {
+      callback(data);
+    });
+  }
+});
