ddada5216b86c970fbddad50c242dbff881e952f
Charles Bido <charlesbido@me.com>
2016-04-20 01:35:36 -0500

    update setup

---
+++ app/controllers/help.js
@@ -0,0 +1,9 @@
+import Ember from 'ember';
+
+export default Ember.Controller.extend({
+  eventEmitter: Ember.inject.service('event-emitter'),
+
+  actions: {
+    
+  }
+});
---
+++ public/docs/howto.md
@@ -0,0 +1,8 @@
+# Set Up
+
+1. Test document
+2. Something else
+
+``` js
+var five = require('johnny-five');
+```
